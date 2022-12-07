fc433bbc08a285b23df2962d25db9b84096bc40f
Chaytan Arora <chaytanarora@gmail.com>
2022-12-07 14:17:29 +0530

    created user model

---
+++ models/User.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const UserSchema = new schema(
+	{
+		googleId: {
+			type: String,
+			required: true,
+		},
+		displayName: {
+			type: String,
+			required: true,
+		},
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+		},
+		mock_test_given: [
+			{
+				title: String,
+				total_questions: Number,
+				questions_attempted: Number,
+				score: Number,
+				wrong_answers: Number,
+			},
+		],
+		quiz_given: [
+			{
+				title: String,
+				total_questions: Number,
+				questions_attempted: Number,
+				score: Number,
+				wrong_answers: Number,
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model("User", UserSchema);
