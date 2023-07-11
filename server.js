42577272a74ce4146f1d5bc76e043e93edbdbe7c
Chaytan Arora <chaytanarora@gmail.com>
2023-07-11 13:47:56 +0530

    config for vercel deployement

--- server.js
+++ server.js
@@ -56,3 +56,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+export default app;
