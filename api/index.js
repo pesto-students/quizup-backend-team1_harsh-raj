8ed1c57190bd78e8ad306be087674e12d773f639
Chaytan Arora <chaytanarora@gmail.com>
2023-07-11 14:32:23 +0530

    vercel config change

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-import app from "../server";
+const app = require("../server");
 
-export default app;
+module.exports = app;
--- server.js
+++ server.js
@@ -57,4 +57,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
-export default app;
+module.exports = app;
